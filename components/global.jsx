048f0c29235da7157298e5818708812ec991614a
selvesan <dev.selvesan@gmail.com>
2017-10-01 10:59:12 +0545

    react bootstrap components used

--- components/global.jsx
+++ components/global.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import {Container,FormControl, FormGroup, InputGroup, Glyphicon} from 'react-bootstrap';
+
 
 class Global extends Component {
     render() {
         return (
-            <div><h1>Global App Component</h1></div>
+            <div className="container">
+
+                <h1>Book Explorer</h1>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl placeholder="Search for a book" type="text" />
+                        <InputGroup.Addon>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+            </div>
         );
     }
 }
